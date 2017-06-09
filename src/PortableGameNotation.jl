47333c751af4ac9743f1978e944829d99a57b578
David Smith <david.smith@gmail.com>
2017-06-09 14:03:39 -0500

    add source and tests

---
+++ src/PortableGameNotation.jl
@@ -0,0 +1,154 @@
+
+module PortableGameNotation
+
+import Base.repr
+
+export readpgn, writepgn, Game, event, site, date, round, white, black, result,
+  whiteelo, blackelo, eventdate, eco, movetext, plycount
+
+mutable struct Game
+  header::Dict{String, String}
+  movetext::String
+end
+
+RESULT_HASH = Dict{AbstractString,Int}("1-0" => 1, "1/2-1/2" => 0, "0-1" => -1)
+REQUIRED_TAGS = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
+DEFAULT_HASH = Dict("Event"=>"","Site"=>"","Date"=>"","Round"=>"","White"=>"",
+  "Black"=>"","Result"=>"")
+
+function Base.repr(g::Game)
+  s = String[]
+  # tags required by standard must be printed first and in order
+  for t in REQUIRED_TAGS
+    push!(s, "[$t \"$(g.header[t])\"]\n")
+  end
+  for k in keys(g.header)
+    if !(k in REQUIRED_TAGS)
+      push!(s, "[$k \"$(g.header[k])\"]\n")
+    end
+  end
+  push!(s, "\n")
+  push!(s, g.movetext)
+  push!(s, "\n")
+  return join(s)
+end
+
+
+function length(g::Game)
+  # length is defined as number of moves
+  moves = split(g.movetext,".")
+  n = length(moves) - 1
+  return n
+end
+
+function validate(g::Game)
+  for t in REQUIRED_TAGS
+    if !(t in g.header)
+      return false
+    end
+  end
+  return true
+end
+
+function query(g::Game, key::String, default="?")
+  try
+    return g.header[key]
+  catch KeyError
+    return default
+  end
+end
+
+white(g::Game) = query(g, "White")
+black(g::Game) = query(g, "Black")
+date(g::Game) = query(g, "Date")
+site(g::Game) = query(g, "Site")
+event(g::Game) = query(g, "Event")
+result(g::Game) = query(g, "Result", "*")
+whiteelo(g::Game) = query(g, "WhiteElo", 0.0)
+blackelo(g::Game) = query(g, "BlackElo", 0.0)
+eco(g::Game) = query(g, "ECO")
+eventdate(g::Game) = query(g, "EventDate")
+plycount(g::Game) = query(g, "PlyCount")
+movetext(g::Game) = g.movetext
+
+
+isdecisive(g::Game) = g.header["Result"] != "1/2-1/2"
+
+const STATE_HEADER = 0
+const STATE_MOVES = 1
+const STATE_NEWGAME = 2
+
+isblank(line) = all(isspace, line)
+
+function readpgn(pgnfilename)
+  f = open(pgnfilename,"r")
+  pgn = readlines(f)
+  close(f)
+  games = Vector{Game}()
+  #g = Game()
+  moves = String[]
+  hdrs = Dict{String,String}()
+  state = STATE_NEWGAME
+  for l in pgn
+    #println("LINE> $l")
+    if ismatch(r"^\[", l)   # header line
+      state = STATE_HEADER
+      fields = split(l,'\"')
+      key = fields[1][2:end-1]
+      val = fields[2]
+      hdrs[key] = val
+    elseif isblank(l) && state == STATE_HEADER
+      state = STATE_MOVES  # TODO: allow for multiple blank lines after header?
+    elseif !isblank(l) && state == STATE_MOVES
+      push!(moves, l)
+    elseif isblank(l) && state == STATE_MOVES
+      push!(games, Game(hdrs, join(moves, " ")))
+      state = STATE_NEWGAME
+    end
+    if state == STATE_NEWGAME
+      moves = String[]
+      hdrs = Dict{String,String}()
+    end
+  end
+  if state == STATE_MOVES
+    push!(games, Game(hdrs, join(moves, " ")))
+  end
+  return games
+end
+
+function sortpgnfile(pgnfilename, outfile)
+  data = readpgn(pgnfilename)
+  datasorted=sort(data, by=cpsort)
+  f = open(outfile, "w")
+  for d in datasorted
+    write(f, repr(d))
+  end
+  close(f)
+end
+
+
+function browsepgn(pgnfilename)
+  data = readpgn(pgnfilename)
+  datasorted = sort(data, by=cpsort)
+  n = 1
+  while true
+    println("=========================== GAME $n ===========================")
+    gamestats(datasorted[n])
+    key = read(STDIN,Char)
+    if key == 'q'
+      break
+    elseif key == 'j'
+      n += 1
+    elseif key == 'k'
+      n -= 1
+    elseif key == 'J'
+      n += 10
+    elseif key == 'K'
+      n -= 10
+    end
+    n = n % length(datasorted)  # wrap around
+  end
+end
+
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,39 @@
+using Base.Test
+
+include("PortableGameNotation.jl")
+using PortableGameNotation
+
+g = readpgn("test.pgn")
+
+print("TESTING on test.pgn ... ")
+
+@test date(g[1]) == "1852.??.??"
+@test site(g[1]) == "Berlin GER"
+@test event(g[1]) == "Berlin GER"
+@test result(g[1]) == "1-0"
+@test white(g[1]) == "Adolf Anderssen"
+@test black(g[1]) == "Jean Dufresne"
+@test whiteelo(g[1]) == "?"
+@test blackelo(g[1]) == "?"
+@test eco(g[1]) == "C52"
+@test eventdate(g[1]) == "?"
+@test plycount(g[1]) == "47"
+@test movetext(g[1]) == """
+1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5 4.b4 { Evans Gambit } Bxb4 5.c3 Ba5 6.d4 exd4 7.O-O d3 8.Qb3 Qf6 9.e5 Qg6 10.Re1 Nge7 11.Ba3 b5 12.Qxb5 Rb8 13.Qa4 Bb6 14.Nbd2 Bb7 15.Ne4 Qf5 16.Bxd3 Qh5 17.Nf6+ gxf6 18.exf6 Rg8 19.Rad1 Qxf3 20.Rxe7+ Nxe7 21.Qxd7+ Kxd7 22.Bf5+ Ke8 23.Bd7+ Kf8 24.Bxe7# 1-0"""
+
+
+@test date(g[2]) == "1851.06.21"
+@test site(g[2]) == "London ENG"
+@test event(g[2]) == "London"
+@test result(g[2]) == "1-0"
+@test white(g[2]) == "Adolf Anderssen"
+@test black(g[2]) == "Lionel Adalbert Bagration Felix Kieseritzky"
+@test whiteelo(g[2]) == "?"
+@test blackelo(g[2]) == "?"
+@test eventdate(g[2]) == "?"
+@test eco(g[2]) == "C33"
+@test plycount(g[2]) == "45"
+@test movetext(g[2]) == """
+1.e4 e5 2.f4 { King's Gambit } exf4 3.Bc4 Qh4+ 4.Kf1 b5 5.Bxb5 Nf6 6.Nf3 Qh6 7.d3 Nh5 8.Nh4 Qg5 9.Nf5 c6 10.g4 Nf6 11.Rg1 cxb5 12.h4 Qg6 13.h5 Qg5 14.Qf3 Ng8 15.Bxf4 Qf6 16.Nc3 Bc5 17.Nd5 Qxb2 18.Bd6 Bxg1 {It is from this move that Black's defeat stems. Wilhelm Steinitz suggested in 1879 that a better move would be 18... Qxa1+; likely moves to follow are 19. Ke2 Qb2 20. Kd2 Bxg1.} 19. e5 Qxa1+ 20. Ke2 Na6 21.Nxg7+ Kd8 22.Qf6+ Nxf6 23.Be7# 1-0"""
+
+println("PASSED")
